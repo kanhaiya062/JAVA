a243ca31d4dee2b804a8fe25cd7214019d1b39ea
Kanhaiya mahto <110100023+kanhaiya062@users.noreply.github.com>
2023-08-30 23:59:13 +0530

    Add files via upload

---
+++ pettern.java
@@ -0,0 +1,19 @@
+public class pettern {
+    public static void main(String[] args) {
+       int m=6;
+       int k=6;
+       /* String p="*";
+        for (int n=0;n<m;n++){
+            System.out.println(p=p+"*");
+        }
+        */
+
+        for (int l=0;l<=m;l++){
+            for (int o=0;o<=k;o++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
