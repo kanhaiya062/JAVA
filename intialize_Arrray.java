5a71b21330bfcf60b6e73b76a53f8334aff6a060
Kanhaiya mahto <110100023+kanhaiya062@users.noreply.github.com>
2023-09-11 23:46:38 +0530

    Add files via upload

---
+++ intialize_Arrray.java
@@ -0,0 +1,11 @@
+public class intialize_Arrray {
+    public static void main(String[] args) {
+        int[] myarr=new int[4];
+        myarr[0]=6;
+        myarr[1]=9;
+        myarr[2]=8;
+        myarr[3]=5;
+        System.out.println(myarr[3]);
+
+    }
+}
