b9ac7307b0196a7de66cf3a9b9c9143b858a62db
Kanhaiya mahto <110100023+kanhaiya062@users.noreply.github.com>
2023-09-27 01:35:48 +0530

    Add files via upload

---
+++ two_D_array_new_method.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class two_D_array_new_method {
+        public static void main(String[] args) {
+            int arr[][]=new int[3][3];
+            Scanner in=new Scanner(System.in);
+            for(int i=0;i<arr.length;i++){
+                for(int j=0;j<arr[i].length;j++){
+                    arr[i][j]=in.nextInt();
+                }
+            }
+           /* for(int i=0;i<arr.length;i++){
+            System.out.println(Arrays.toString(arr[i]+" "));
+           }
+            */ 
+            for(int[] a:arr){
+                System.out.println(Arrays.toString(a));
+            }
+        }
+        }
+    
+    
+
