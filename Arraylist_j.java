9d1dc0225241476a887ce5760cb2ee05a4f8a042
Kanhaiya mahto <110100023+kanhaiya062@users.noreply.github.com>
2023-09-30 01:57:21 +0530

    Add files via upload

---
+++ Arraylist_j.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class Arraylist_j {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList();
+        list.add(90);
+        list.add(80);
+        list.add(89);
+        list.add(23);
+        list.add(45);
+        System.out.println(list);
+    }
+}
+    
+
